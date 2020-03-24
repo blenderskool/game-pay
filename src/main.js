3d6bb51e8e56642ad8bfdd8091efc0f36c008542
blenderskool <akash.hamirwasia@gmail.com>
2020-03-24 23:50:41 +0530

    Setup tailwindcss

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import '@/assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 
