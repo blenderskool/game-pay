1492f9d1ec177630f086a65fe5cd1ba25723b0d8
blenderskool <akash.hamirwasia@gmail.com>
2020-04-11 21:22:26 +0530

    Add inner shadow to selected MegaChoice
    Also move dynamic classes from computed properties to the template

--- src/components/MegaChoice.vue
+++ src/components/MegaChoice.vue
@@ -1,5 +1,11 @@
 <template>
-  <label :class="`bg-gray-800 block text-center rounded mr-2 w-full py-5 shadow-lg border-solid text-lg cursor-pointer ${style} ${value === inputValue && activeStyle}`">
+  <label
+    :class="
+      `bg-gray-800 block text-center rounded mr-2 w-full py-5 shadow-lg border-solid text-lg cursor-pointer
+      ${this.overrideClass || 'text-blue-400 border-blue-400'}
+      ${value === inputValue && (this.activeClass || 'border-2 shadow-inner')}`
+    "
+  >
     <slot />
     <input @input="handleInput" class="hidden" type="radio" :name="name" :value="inputValue">
   </label>
@@ -20,18 +26,6 @@ export default {
     },
     activeClass: String,
     overrideClass: String,
-  },
-  computed: {
-    activeStyle() {
-      if (!this.activeClass) return 'border-2';
-
-      return this.activeClass;
-    },
-    style() {
-      if (!this.overrideClass) return 'text-blue-400 border-blue-400';
-
-      return this.overrideClass;
-    }
   },
   methods: {
     handleInput(e) {
